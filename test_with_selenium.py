dbdde26fae04ae26e2b80e67b229a3f902c04f93
n1cks1 <nikitasib015@icloud.com>
2025-03-06 14:57:21 +0300

    добавил сбор дат с google patents

--- test_with_selenium.py
+++ test_with_selenium.py
@@ -14,14 +14,29 @@ try:
         EC.presence_of_element_located((By.CLASS_NAME, 'result.style-scope.search-result-item'))
     )
 
+    text_in_patent = []
+
     results = driver.find_elements(By.CLASS_NAME, 'result.style-scope.search-result-item')
+
+    for elements in results:
+        elem = elements.find_element(By.XPATH, '//div[contains(@class, "search-result-item")]//raw-html[contains(@class, "search-result-item")]//span[contains(@class, "style-scope")]').text
+        text_in_patent.append(elem)
+
+    print(text_in_patent)
+
 
     for patent in results:
         title = patent.find_element(By.CSS_SELECTOR, '.style-scope.raw-html').text
 
         name = patent.find_element(By.CSS_SELECTOR, '.bullet-before.style-scope.search-result-item .style-scope.search-result-item .style-scope.raw-html').text
 
-        print(f'Название патента: {title},\n name autor patents: {name}\n\n')
+        date = patent.find_element(By.XPATH, '//h4[contains(@class, "dates")]').text
+
+            # text_in_patent = patent.find_element(By.XPATH, '//div[contains(@class, "search-result-item")]//raw-html[contains(@class, "search-result-item")]//span[contains(@class, "style-scope")]').text
+
+
+
+        # print(f'Title: {title},\nName autor patents: {name}\nDate: {date} \n\n')
 
 finally:
     # Закрытие браузера
